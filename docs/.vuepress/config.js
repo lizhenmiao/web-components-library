7a33b796df5788894c85f0039fb5ac28a8386269
lizhenmiao <431521978@qq.com>
2025-04-11 16:30:01 +0800

    vuepress 配置调整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,36 +1,29 @@
 module.exports = {
-  title: '我的Web Components库',
-  description: '原生Web Components组件库文档',
-
+  title: '我的 Web Components 库',
+  description: '原生 Web Components 组件库文档',
   // 设置基础路径，必须设置为仓库名才能在GitHub Pages正常工作
-  base: '/web-components-library/',
-
+  base: '/web-components-library',
   themeConfig: {
     // 导航栏
     nav: [
-      { text: '首页', link: '/' },
-      { text: '组件', link: '/components/modal-box' },
-      { text: 'GitHub', link: 'https://github.com/yourusername/web-components-library' }
+      { text: '组件', link: '/components/modal-box' }
     ],
-
     // 侧边栏
     sidebar: [
       {
         title: '组件',
-        collapsable: false,
+        collapsable: true,
         children: [
           '/components/modal-box'
         ]
       }
     ],
-
     // 社交链接
-    repo: 'https://github.com/yourusername/web-components-library',
-
+    repo: 'https://github.com/lizhenmiao/web-components-library',
     // 页脚
     lastUpdated: '上次更新',
+    // 贡献者
     contributors: false,
-
     // 编辑链接
     editLinks: false
   }
